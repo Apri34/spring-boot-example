8d4df935c5712f8e1a38f8ffa74f1e0c99be4856
andreaspribitzer <andreas.pribitzer@outlook.at>
2021-11-25 16:51:50 +0100

    Modify to publish on Stackoverflow

--- src/main/java/com/apri34/ExampleApplication.java
+++ src/main/java/com/apri34/ExampleApplication.java
@@ -2,9 +2,16 @@ package com.apri34;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ExampleApplication {
+public class ExampleApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ExampleApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(ExampleApplication.class);
     }
